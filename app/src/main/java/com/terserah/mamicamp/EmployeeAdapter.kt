13326b8be4ad305cb5ba0d0bf75897b04a769789
Dedi Dot <hello.dedidot@gmail.com>
2019-03-14 20:12:04 +0700

    up

--- app/src/main/java/com/terserah/mamicamp/EmployeeAdapter.kt
+++ app/src/main/java/com/terserah/mamicamp/EmployeeAdapter.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.layout_main_activity_constraint.view.*
+import com.bumptech.glide.Glide
+
+
 
 class EmployeeAdapter(val activity: Activity, val data: ArrayList<EmployeePojo>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -29,9 +32,17 @@ class EmployeeAdapter(val activity: Activity, val data: ArrayList<EmployeePojo>)
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(employeePojo : EmployeePojo ) {
-            itemView.TextView1.text = "${employeePojo.employeeName}"
-            itemView.TextView2.text = "${employeePojo.employeeAge}"
-            itemView.TextView3.text = "${employeePojo.employeeSalary}"
+            val url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwshxYpeEpWXzlZmJeUYHL0G-avK5txosu7pND_md-o_mY0n3a"
+
+            Glide
+                .with(itemView.context)
+                .load(url)
+                .centerCrop()
+                .into(itemView.imageView1)
+
+            itemView.TextView1.text = "Nama : ${employeePojo.employeeName}"
+            itemView.TextView2.text = "Umur : ${employeePojo.employeeAge}"
+            itemView.TextView3.text = "Rp ${employeePojo.employeeSalary}"
             itemView.ButtonList.setOnClickListener {
                 Log.e("datane name", "${employeePojo.employeeName}")
                 Log.e("datane salary", "${employeePojo.employeeSalary}")
