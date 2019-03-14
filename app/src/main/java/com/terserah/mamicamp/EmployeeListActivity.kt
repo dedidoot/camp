f29fb9fc24727d1c19e206102cf0d3e71db14909
Dedi Dot <hello.dedidot@gmail.com>
2019-03-14 15:51:51 +0700

    setup api

--- app/src/main/java/com/terserah/mamicamp/EmployeeListActivity.kt
+++ app/src/main/java/com/terserah/mamicamp/EmployeeListActivity.kt
@@ -2,12 +2,18 @@ package com.terserah.mamicamp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.httpDelete
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.layout_employe_list.*
 
 class EmployeeListActivity : AppCompatActivity() {
 
-    var adapterBebas : EmployeeAdapter ?= null
-    var datanya : MutableList<EmployeePojo> = arrayListOf()
+    var adapterBebas: EmployeeAdapter? = null
+    var datanya: MutableList<EmployeePojo> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,15 +21,35 @@ class EmployeeListActivity : AppCompatActivity() {
         adapterBebas = EmployeeAdapter(this, datanya)
         recyclerEmployee.adapter = adapterBebas
 
-        for (i in 0 until 100){
+        for (i in 0 until 100) {
             datanya.add(
-                EmployeePojo("Nama $i",
-                "umur $i", "Salary $i")
+                EmployeePojo(
+                    "Nama $i",
+                    "umur $i", "Salary $i"
+                )
             )
         }
 
         adapterBebas?.notifyDataSetChanged()
 
+        getData()
+
+    }
+
+    private fun getData() {
+        val baseUrlGet = "http://dummy.restapiexample.com/api/v1/employees"
+        baseUrlGet
+            .httpGet()
+            .responseString { request, response, result ->
+
+                val listType = object : TypeToken<ArrayList<EmployeePojo>>() {
+                }.type
+
+                datanya = Gson().fromJson(result.component1(), listType)
+
+                Log.e("response", "${datanya.size}")
+                Log.e("response", "${datanya[0].employeeName}")
+            }
     }
 
 }
--- app/src/main/java/com/terserah/mamicamp/EmployeePojo.kt
+++ app/src/main/java/com/terserah/mamicamp/EmployeePojo.kt
@@ -1,7 +1,12 @@
 package com.terserah.mamicamp
 
-class EmployeePojo(
-    var employeeName: String? = null,
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class EmployeePojo(
+    @SerializedName("employee_name") var employeeName: String? = null,
     var employeeAge: String? = null,
     var employeeSalary: String? = null
-)
+) : Parcelable
